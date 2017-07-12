a1121801eb667510027d58d375aef4778ee096e8
Dmitriy Makux <mtmato6@gmail.com>
2017-07-12 22:03:41 +0300

    Init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://buss.test/admin/');
+define('HTTP_CATALOG', 'http://buss.test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://buss.test/admin/');
+define('HTTPS_CATALOG', 'http://buss.test/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/buss.test/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/buss.test/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/buss.test/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/buss.test/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/buss.test/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/buss.test/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/buss.test/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/buss.test/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/buss.test/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/buss.test/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/buss.test/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/buss.test/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'buss.test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
